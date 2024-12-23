e144223d2eb1e35d51a59d2a4069f5a585686b18
sungjae1825 <50562582+sungjae1825@users.noreply.github.com>
2024-12-23 20:41:31 +0900

    init

---
+++ Source/TPCPP/TPCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPCPP_API ATPCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
